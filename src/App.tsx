b9f8a3091922036dcda91083500c1596d19d118c
Moha-Sameh <ms.slama01@hotmail.com>
2021-07-05 14:38:23 +0200

    Add Packages, File structure, Auth store, Instace

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from "react";
+import LoginPage from "./pages/LoginPage";
 
 const App: React.FC = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <p>Mohamed</p>
+        <LoginPage />
       </header>
     </div>
   );
---
+++ src/store/authStore.ts
@@ -0,0 +1,60 @@
+import { makeAutoObservable } from "mobx";
+import instance from "./instance";
+import decode from "jwt-decode";
+
+class AuthStore {
+  user = null;
+  loading = true;
+  constructor() {
+    makeAutoObservable(this);
+  }
+  setUser = (token: string) => {
+    localStorage.setItem("myToken", token);
+    this.user = decode(token);
+    instance.defaults.headers.common.Authorization = `Bearer ${token}`;
+  };
+
+  signup = async (userData: { [x: string]: string | Blob }) => {
+    try {
+      const formData = new FormData();
+      for (const key in userData) formData.append(key, userData[key]);
+      const response = await instance.post("/register", formData);
+      this.setUser(response.data.token);
+    } catch (error) {
+      throw new Error(" Invalid request");
+    }
+  };
+
+  signin = async (userData: any) => {
+    try {
+      const response = await instance.post("/login", userData);
+      this.setUser(response.data.token);
+      this.loading = false;
+    } catch (error) {
+      throw new Error(" Invalid request");
+    }
+  };
+
+  signout = () => {
+    this.user = null;
+    localStorage.removeItem("myToken");
+    localStorage.removeItem("count");
+    delete instance.defaults.headers.common.Authorization;
+  };
+  checkForToken = () => {
+    const token = localStorage.getItem("myToken");
+    if (token) {
+      const currentTime = Date.now();
+      const user: any = decode(token);
+      if (user.exp >= currentTime) {
+        this.setUser(token);
+      } else {
+        this.signout();
+      }
+    }
+  };
+}
+
+const authStore = new AuthStore();
+authStore.checkForToken();
+export default authStore;
