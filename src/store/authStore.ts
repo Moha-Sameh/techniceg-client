ca402b431943c5a74ced958ed0374e3fa1728ac7
Moha-Sameh <ms.slama01@hotmail.com>
2021-07-07 14:36:26 +0200

    Add refreshToken to authStore & axios interceptors

--- src/store/authStore.ts
+++ src/store/authStore.ts
@@ -2,17 +2,54 @@ import { makeAutoObservable } from "mobx";
 import instance from "./instance";
 import decode from "jwt-decode";
 
+const TOKEN = "myToken";
+const REFRESH_TOKEN = "myRefreshToken";
+
+interface ITokenResponse {
+  token: string;
+  refreshToken: string;
+}
+
 class AuthStore {
   user = null;
-  loading = true;
 
   constructor() {
     makeAutoObservable(this);
   }
-  setUser = (token: string) => {
-    localStorage.setItem("myToken", token);
+
+  setUser = ({ token, refreshToken }: ITokenResponse) => {
+    this.setTokens(token, refreshToken);
     instance.defaults.headers.common.Authorization = `Bearer ${token}`;
     this.user = decode(token);
+  };
+
+  //User tokens
+  setTokens = (token: string, refreshToken: string) => {
+    localStorage.setItem(TOKEN, token);
+    localStorage.setItem(REFRESH_TOKEN, refreshToken);
+  };
+
+  getToken = () => {
+    return localStorage.getItem(TOKEN);
+  };
+
+  getRefreshToken = () => {
+    return localStorage.getItem(REFRESH_TOKEN);
+  };
+
+  removeTokens = () => {
+    localStorage.removeItem(TOKEN);
+    localStorage.removeItem(REFRESH_TOKEN);
+  };
+
+  refreshToken = async () => {
+    try {
+      const refreshToken = this.getRefreshToken();
+      const response = await instance.post("/refresh", refreshToken);
+      this.setTokens(response.data.token, response.data.refreshToken);
+    } catch (error) {
+      console.error(error);
+    }
   };
 
   signup = async (userData: { [x: string]: string | Blob }) => {
@@ -20,42 +57,53 @@ class AuthStore {
       const formData = new FormData();
       for (const key in userData) formData.append(key, userData[key]);
       const response = await instance.post("/register", formData);
-      this.setUser(response.data.token);
+      this.setUser(response.data);
     } catch (error) {
-      throw new Error(" Invalid request");
+      console.error(error);
     }
   };
 
   signin = async (userData: any) => {
     try {
       const response = await instance.post("/login", userData);
-      this.setUser(response.data.token);
-      this.loading = false;
+      this.setUser(response.data);
     } catch (error) {
-      console.log(error);
+      console.error(error);
     }
   };
 
   signout = () => {
     this.user = null;
-    this.loading = true;
-    localStorage.removeItem("myToken");
+    this.removeTokens();
     delete instance.defaults.headers.common.Authorization;
   };
+
   checkForToken = () => {
-    const token = localStorage.getItem("myToken");
+    const token = this.getToken();
     if (token) {
       const currentTime = Date.now();
-      const user: any = decode(token);
-      if (user.exp >= currentTime) {
-        this.setUser(token);
-      } else {
-        this.signout();
+      const decodedToken: any = decode(token);
+      if (decodedToken.exp < currentTime) {
+        this.checkForRefreshToken();
       }
     }
+  };
+
+  checkForRefreshToken = () => {
+    const refreshToken = this.getRefreshToken();
+    if (refreshToken) {
+      const currentTime = Date.now();
+      const decodedRefreshToken: any = decode(refreshToken);
+      if (decodedRefreshToken.exp > currentTime) {
+        this.refreshToken();
+        return;
+      }
+    }
+    this.signout();
   };
 }
 
 const authStore = new AuthStore();
 authStore.checkForToken();
+
 export default authStore;
--- src/store/instance.ts
+++ src/store/instance.ts
@@ -1,6 +1,19 @@
 import axios from "axios";
+import authStore from "./authStore";
+
 const instance = axios.create({
   baseURL: "http://localhost:8080",
+});
+
+// Interceptor it return reject 401
+instance.interceptors.response.use(undefined, function (err) {
+  const orginalRequest = err.config;
+  if (err.response.status === 401 && !orginalRequest._retry) {
+    orginalRequest._retry = true;
+    authStore.checkForToken();
+    return instance(orginalRequest);
+  }
+  return Promise.reject(err);
 });
 
 export default instance;
